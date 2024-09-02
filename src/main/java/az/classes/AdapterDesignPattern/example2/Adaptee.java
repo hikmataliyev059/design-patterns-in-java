2baf5a941fa51f85530bb0e8658fb2f93569e377
hikmet059 <javahikmet5909@gmail.com>
2024-09-02 16:28:01 +0400

    adapter design pattern task

---
+++ src/main/java/az/classes/AdapterDesignPattern/example2/Adaptee.java
@@ -0,0 +1,9 @@
+package az.classes.AdapterDesignPattern.example2;
+
+// Adaptee class with a different interface
+// Uyğun olmayan interfeysi olan mövcud sinif.
+public class Adaptee {
+    void specificRequest() {
+        System.out.println("Called specificRequest()");
+    }
+}
